b23150c4e9447dd6b49b1310562d4452ec861646
Merlin Loison <Darkadok@gmail.com>
2015-03-23 18:09:43 +0100

    Add writting in files at closing (not ended => comment)

--- src/Gestion.c
+++ src/Gestion.c
@@ -3,7 +3,7 @@ int nombrePointVirguleDansLigne(FILE* fichier)
 {
 	char lettre = NULL;
 	int nombre=0;
-	while (lettre != '\n')//premiere ligne, cas particulier
+	while (lettre != '\n')//premiere ligne, cas general
 	{
 		fscanf(fichier, "%c", &lettre);
 		if (lettre == ';')
@@ -71,8 +71,7 @@ void lectureFichierDepartement(Region** tab_region, int** taille_tab_region)// =
 				ref_tab_departement = &(((*tab_region) + i) -> tab_departement);
 				taille_tab_departement = ((*tab_region) + i)->taille_tab_departement;//plantouile à mort
 				ref_taille_tab_departement = &(((*tab_region) + i)->taille_tab_departement);
-				
-				i++;
+
 			}
 
 			
@@ -96,6 +95,7 @@ void lectureFichierDepartement(Region** tab_region, int** taille_tab_region)// =
 
 
 			lettre = fgetc(fichier);
+			i++;
 		}
 
 		fclose(fichier);
@@ -123,7 +123,7 @@ void lectureFichierRecensements()
 	else
 	{
 		
-		nombre_recensements -= 2;
+		nombre_recensements = nombrePointVirguleDansLigne(fichier) - 2;
 		rewind(fichier);
 		fgets(ligne_en_cours, sizeof(ligne_en_cours), fichier);
 
@@ -138,15 +138,14 @@ void lectureFichierRecensements()
 }
 
 
-
-ecritureFichierRecensements()
-{
-	return 0;
-}
-
 void ecritureFichierDepartement(Region* tab_region, int* taille_tab_region)
 {
 	FILE* fichier = NULL;
+	int i;
+	int no_dep;
+	char nom_dep[200];
+	char prefecture[200];
+	char nom_region[200];
 #if _DEBUG
 	fichier = fopen("../../../../GIT/Recensement/Import/departements.csv", "w+");
 #endif
@@ -157,4 +156,53 @@ void ecritureFichierDepartement(Region* tab_region, int* taille_tab_region)
 	{
 		return NULL;
 	}
+	else
+	{
+		for (i = 0; i < *taille_tab_region; i++)
+		{	
+			/*no_dep = *tab_region ->;
+			nom_dep = tab_region;
+			prefecture = tab_region;
+			nom_region = (*(tab_region + i) ->nom_reg);   => i don't fucking know what i'm supposed to do here*/
+		
+			fprintf(fichier, "%d;%s;%s;%s\n", no_dep, nom_dep, prefecture, nom_region);
+		}
+		fclose(fichier);
+	}
+	
+}
+
+void ecritureFichierRecensements(Region* tab_region, int* taille_tab_region)
+{
+	FILE* fichier = NULL;
+	int i;
+#if _DEBUG
+	fichier = fopen("../../../../GIT/Recensement/Import/recensements.csv", "w+");
+#endif
+#if !_DEBUG
+	fichier = fopen("../Import/recensements.csv", "w+");
+#endif
+	if (fichier == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		int nombre_recensements = nombrePointVirguleDansLigne(fichier) - 2;
+		fprintf(fichier, "%s;%s;%s;", "DEPCOM","DEP","LIBMIN");//cas de la premiere ligne
+		for (i = 0; i < nombre_recensements; i++)
+		{
+			fprintf(fichier, "%s;", "Recemement + i ");//Ou est stocke les années de recensement ?
+		}
+		for (i = 0; i < "nombre ville total"; i++)//comment connaitre le nombre de ville au total?
+		{
+			fprintf(fichier, "\n%d;%d;%s;", "dep_com + i", "numero_dep +i", "nom_ville + i");//Ou sont placées ces merdes..
+			for (i = 0; i < nombre_recensements; i++)
+			{
+				fprintf(fichier, "%s;", "Recemement + i ");//même combat
+			}
+		}
+		fclose(fichier);
+	}
+	
 }
--- src/Gestion.h
+++ src/Gestion.h
@@ -11,5 +11,5 @@ int nombrePointVirguleDansLigne(FILE* fichier);
 void lectureFichierDepartement(Region** tab_region, int** taille_tab_region);
 void ecritureFichierDepartement(Region* tab_region, int* taille_tab_region);
 void lectureFichierRecensements();
-int ecritureFichierRecensements();
+void ecritureFichierRecensements();
 #endif
